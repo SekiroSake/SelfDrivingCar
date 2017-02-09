17da9d201efe42d2076c2f7125f73ea0957b329a
Hang Wang <Hang Wang>
2017-02-09 16:38:47 -0500

    sigmoid is about input values

--- MiniFlow/nn.py
+++ MiniFlow/nn.py
@@ -1,32 +1,32 @@
 """
-The setup is similar to the prevous `Linear` node you wrote
-except you're now using NumPy arrays instead of python lists.
+This network feeds the output of a linear transform
+to the sigmoid function.
 
-Update the Linear class in miniflow.py to work with
-numpy vectors (arrays) and matrices.
+Finish implementing the Sigmoid class in miniflow.py!
 
-Test your code here!
+Feel free to play around with this network, too!
 """
 
 import numpy as np
 from miniflow import *
 
-X, W, b = Input(), Input(), Input()
+inputs, weights, bias = Input(), Input(), Input()
 
-f = Linear(X, W, b)
+f = Linear(inputs, weights, bias)
+g = Sigmoid(f)
 
-X_ = np.array([[-1., -2.], [-1, -2]])
-W_ = np.array([[2., -3], [2., -3]])
-b_ = np.array([-3., -5])
+x = np.array([[-1., -2.], [-1, -2]])
+w = np.array([[2., -3], [2., -3]])
+b = np.array([-3., -5])
 
-feed_dict = {X: X_, W: W_, b: b_}
+feed_dict = {inputs: x, weights: w, bias: b}
 
 graph = topological_sort(feed_dict)
-output = forward_pass(f, graph)
+output = forward_pass(g, graph)
 
 """
 Output should be:
-[[-9., 4.],
-[-9., 4.]]
+[[  1.23394576e-04   9.82013790e-01]
+ [  1.23394576e-04   9.82013790e-01]]
 """
 print(output)
