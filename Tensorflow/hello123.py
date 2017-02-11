36d1feb938b6dc27fc59b65696ac801945bbdd98
Hang Wang <Hang Wang>
2017-02-11 15:57:43 -0500

    tf

--- Tensorflow/hello123.py
+++ Tensorflow/hello123.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
 
+hello_constant = tf.constant('Hello World!')
+with tf.Session() as sess:
+    # Run the tf.constant operation in the session
+    output = sess.run(hello_constant)
+    print(output)
+    
 def run():
     output = None
     x = tf.placeholder(tf.int32)
