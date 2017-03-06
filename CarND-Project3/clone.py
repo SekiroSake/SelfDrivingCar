fd5a216f2844f192e95b7056ac822ec435edea00
Adam Wang <hwang12wou@gmail.com>
2017-03-05 23:06:56 -0500

    file define problem

--- CarND-Project3/clone.py
+++ CarND-Project3/clone.py
@@ -3,7 +3,7 @@ import cv2
 import  numpy as np
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
